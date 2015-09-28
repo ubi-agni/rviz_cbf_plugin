2661d9460d6432a5edcae529f896d405760ec6c3
mingless <ming.bezlitosny@gmail.com>
2015-09-28 13:58:57 +0200

    Added InteractiveMarker, unresolved issue with boost::shared_ptr usage in InteractiveMarkerFeedbackConstPtr.

--- test/run_cbf.cpp
+++ test/run_cbf.cpp
@@ -28,8 +28,8 @@
 #include <kdl/frames.hpp>
 #include <boost/program_options.hpp>
 #include <boost/make_shared.hpp>
-#include <boost/assign/list_of.hpp>
 
+#include <boost/assign/list_of.hpp>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 #include <moveit/robot_model/robot_model.h>
@@ -39,7 +39,113 @@
 #include <moveit/rdf_loader/rdf_loader.h>
 #include <angles/angles.h>
 
+#include <interactive_markers/interactive_marker_server.h>
+
 namespace po = boost::program_options;
+namespace vm = visualization_msgs;
+
+boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server;
+
+vm::InteractiveMarkerFeedback marker_feedback;
+
+void processFeedback( const vm::InteractiveMarkerFeedbackConstPtr &feedback )
+{
+	marker_feedback.pose.position.x = feedback.pose.position.x;
+	marker_feedback.pose.position.y = feedback.pose.position.y;
+	marker_feedback.pose.position.z = feedback.pose.position.z;
+	
+	marker_feedback.pose.orientation.w = feedback.pose.orientation.w;
+	marker_feedback.pose.orientation.x = feedback.pose.orientation.x;
+	marker_feedback.pose.orientation.y = feedback.pose.orientation.y;
+	marker_feedback.pose.orientation.z = feedback.pose.orientation.z;
+	
+	marker_feedback.header.frame_id = feedback.header.frame_id;
+	marker_feedback.header.stamp = feedback.header.stamp;
+	
+//TEMP -> pending clean up
+	
+  std::ostringstream s;
+  s << "Feedback from marker '" << feedback->marker_name << "' "
+      << " / control '" << feedback->control_name << "'";
+
+  std::ostringstream mouse_point_ss;
+  if( feedback->mouse_point_valid )
+  {
+    mouse_point_ss << " at " << feedback->mouse_point.x
+                   << ", " << feedback->mouse_point.y
+                   << ", " << feedback->mouse_point.z
+                   << " in frame " << feedback->header.frame_id;
+  }
+
+  switch ( feedback->event_type )
+  {
+    case visualization_msgs::InteractiveMarkerFeedback::BUTTON_CLICK:
+      ROS_INFO_STREAM( s.str() << ": button click" << mouse_point_ss.str() << "." );
+      break;
+
+    case visualization_msgs::InteractiveMarkerFeedback::MENU_SELECT:
+      ROS_INFO_STREAM( s.str() << ": menu item " << feedback->menu_entry_id << " clicked" << mouse_point_ss.str() << "." );
+      break;
+
+    case visualization_msgs::InteractiveMarkerFeedback::POSE_UPDATE:
+      ROS_INFO_STREAM( s.str() << ": pose changed"
+          << "\nposition = "
+          << feedback->pose.position.x
+          << ", " << feedback->pose.position.y
+          << ", " << feedback->pose.position.z
+          << "\norientation = "
+          << feedback->pose.orientation.w
+          << ", " << feedback->pose.orientation.x
+          << ", " << feedback->pose.orientation.y
+          << ", " << feedback->pose.orientation.z
+          << "\nframe: " << feedback->header.frame_id
+          << " time: " << feedback->header.stamp.sec << "sec, "
+          << feedback->header.stamp.nsec << " nsec" );
+      break;
+
+    case visualization_msgs::InteractiveMarkerFeedback::MOUSE_DOWN:
+      ROS_INFO_STREAM( s.str() << ": mouse down" << mouse_point_ss.str() << "." );
+      break;
+
+    case visualization_msgs::InteractiveMarkerFeedback::MOUSE_UP:
+      ROS_INFO_STREAM( s.str() << ": mouse up" << mouse_point_ss.str() << "." );
+      break;
+  }
+
+  server->applyChanges();
+}
+
+vm::Marker makeBox( vm::InteractiveMarker &msg )
+{
+  vm::Marker marker;
+
+  marker.type = vm::Marker::CUBE;
+  marker.scale.x = msg.scale * 0.45;
+  marker.scale.y = msg.scale * 0.45;
+  marker.scale.z = msg.scale * 0.45;
+  marker.color.r = 0.5;
+  marker.color.g = 0.5;
+  marker.color.b = 0.5;
+  marker.color.a = 1.0;
+
+  return marker;
+}
+
+vm::InteractiveMarkerControl& makeBoxControl( vm::InteractiveMarker &msg )
+{
+  vm::InteractiveMarkerControl control;
+  control.always_visible = true;
+  control.markers.push_back( makeBox(msg) );
+  msg.controls.push_back( control );
+
+  return msg.controls.back();
+}
+
+void saveMarker( vm::InteractiveMarker int_marker )
+{
+  server->insert(int_marker);
+  server->setCallback(int_marker.name, &processFeedback);
+}
 
 static void usage (const char* prog_name, const po::options_description &opts) {
 	std::cout << "usage: Server [options] FILES" << std::endl;
@@ -122,6 +228,85 @@ void update_message(sensor_msgs::JointState &msg,
 	wrapper = resource->get();
 }
 
+//////////////////////////
+
+void make6DofMarker( bool fixed, unsigned int interaction_mode, const tf::Vector3 &position, bool show_6dof )
+{
+  vm::InteractiveMarker int_marker;
+  int_marker.header.frame_id = "base_link";
+  tf::pointTFToMsg(position, int_marker.pose.position);
+  int_marker.scale = 1;
+
+  int_marker.name = "6dof marker";
+  int_marker.description = "6-DOF Marker Control";
+
+  // insert a box
+  makeBoxControl(int_marker);
+  int_marker.controls[0].interaction_mode = interaction_mode;
+
+  vm::InteractiveMarkerControl control;
+
+  if ( fixed )
+  {
+    int_marker.name += "_fixed";
+    int_marker.description += "\n(fixed orientation)";
+    control.orientation_mode = vm::InteractiveMarkerControl::FIXED;
+  }
+
+  if (interaction_mode != visualization_msgs::InteractiveMarkerControl::NONE)
+  {
+      std::string mode_text;
+      if( interaction_mode == visualization_msgs::InteractiveMarkerControl::MOVE_3D )         mode_text = "MOVE_3D";
+      if( interaction_mode == visualization_msgs::InteractiveMarkerControl::ROTATE_3D )       mode_text = "ROTATE_3D";
+      if( interaction_mode == visualization_msgs::InteractiveMarkerControl::MOVE_ROTATE_3D )  mode_text = "MOVE_ROTATE_3D";
+      int_marker.name += "_" + mode_text;
+      int_marker.description = std::string("3D Control") + (show_6dof ? " + 6-DOF controls" : "") + "\n" + mode_text;
+  }
+
+  if(show_6dof)
+  {
+    control.orientation.w = 1;
+    control.orientation.x = 1;
+    control.orientation.y = 0;
+    control.orientation.z = 0;
+    control.name = "rotate_x";
+    control.interaction_mode = vm::InteractiveMarkerControl::ROTATE_AXIS;
+    int_marker.controls.push_back(control);
+    control.name = "move_x";
+    control.interaction_mode = vm::InteractiveMarkerControl::MOVE_AXIS;
+    int_marker.controls.push_back(control);
+
+    control.orientation.w = 1;
+    control.orientation.x = 0;
+    control.orientation.y = 1;
+    control.orientation.z = 0;
+    control.name = "rotate_z";
+    control.interaction_mode = vm::InteractiveMarkerControl::ROTATE_AXIS;
+    int_marker.controls.push_back(control);
+    control.name = "move_z";
+    control.interaction_mode = vm::InteractiveMarkerControl::MOVE_AXIS;
+    int_marker.controls.push_back(control);
+
+    control.orientation.w = 1;
+    control.orientation.x = 0;
+    control.orientation.y = 0;
+    control.orientation.z = 1;
+    control.name = "rotate_y";
+    control.interaction_mode = vm::InteractiveMarkerControl::ROTATE_AXIS;
+    int_marker.controls.push_back(control);
+    control.name = "move_y";
+    control.interaction_mode = vm::InteractiveMarkerControl::MOVE_AXIS;
+    int_marker.controls.push_back(control);
+  }
+
+  server->insert(int_marker);
+  server->setCallback(int_marker.name, &processFeedback);
+  if (interaction_mode != visualization_msgs::InteractiveMarkerControl::NONE)
+    menu_handler.apply( *server, int_marker.name );
+}
+
+//////////////////////////
+
 int main(int argc, char *argv[]) {
 	std::string tip_frame;
 	ros::init (argc, argv, "run_cbf_test");
@@ -160,22 +345,31 @@ int main(int argc, char *argv[]) {
 	auto jsp = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
 	// init joint_state message
 	auto js_msg = init_message(kdl_chain);
+	
+	
+	////////////////////////////////////////////////
+	
+	server.reset( new interactive_markers::InteractiveMarkerServer("basic_controls","",false) );
+	
+	tf::Vector3 init_position = tf::Vector3(target.segment(0,3));
+	tf::Quaternion init_orientation = tf::Quaternion(target.segment(3,4));
+//TEMP -> check validity
+	make6DofMarker( false, visualization_msgs::InteractiveMarkerControl::NONE, init_position, true );
+	
+	
+	////////////////////////////////////////////////
 
 
 	// run controller
 	ros::Rate rate(50); // 50 hz update rate
 	while (ros::ok()) {
-		// do some nice target motion
-		double t = ros::Time::now().toSec();
-		double w = 2*M_PI / 10; // motion frequency for period 10s
-		KDL::Frame tm(KDL::Rotation::EulerZYX(angles::from_degrees(-170),
-		                                      angles::from_degrees(10),
-		                                      angles::from_degrees(-30)),
-		              KDL::Vector(0.2 * sin(w*t), 0.5 + 0.2 * cos(w*t), 0.1));
+		
+		
+		KDL::Frame tm(KDL::Rotation::Quaternion(marker_feedback.pose.orientation),
+		              KDL::Vector(marker_feedback.pose.position);
 		target_vector.head(3) = Eigen::Map<Eigen::Vector3d>(tm.p.data);
 		target_vector.tail(3) = Eigen::Map<Eigen::Vector3d>(tm.M.GetRot().data);
-		// TODO: replace the target motion computation
-		// by feedback from interactive marker
+
 		target->set_reference(target_vector);
 
 		// perform controller step
@@ -185,10 +379,17 @@ int main(int argc, char *argv[]) {
 		update_message(js_msg, joints);
 		jsp.publish(js_msg);
 
+//
+		// ?
+		server.applyChanges();
+//
+
 		// process ros messages
 		ros::spinOnce();
 		rate.sleep();
 	}
+	
+	server.reset();
 
 	return EXIT_SUCCESS;
 }
